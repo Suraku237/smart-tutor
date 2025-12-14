4ec95fcc7474ecad793ec075fb629d90b3e27f08
Izuchukwu271 <echijioke483@gmail.com>
2025-12-14 12:15:00 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
-import 'screens/onboarding.dart';
-// ignore: unused_import
-import 'screens/home_screen.dart';
 
 
 void main() {
-  runApp(const MyApp());
+  runApp(const SmartTutorApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SmartTutorApp extends StatelessWidget {
+  const SmartTutorApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      title: 'Smart Tutor',
       debugShowCheckedModeBanner: false,
-      initialRoute: '/',
-      routes: {
-        '/': (context) => const SplashScreen(),
-         '/onboarding': (context) => const OnboardingScreen(),
-        '/home': (context) => const HomePage(),
-      },
+
+      // App Theme (You can change colors later)
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Colors.white,
+        fontFamily: 'Roboto',
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)
+      ),
+
+      // First screen to load
+      home: SplashScreen(),
     );
   }
 }
-
